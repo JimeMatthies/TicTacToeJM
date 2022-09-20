65fc3fa25eb4a39a805f337ece8a84d3cb8d32db
Jime Matthies <JimeMatthies@gmail.com>
2022-09-20 15:26:48 -0300

    Tic Tac Toe JM

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import { whoIsWinner } from "./helper";
+import Board from './components/Board'
 import Footer from './components/Footer';
-import Announcement from './components/Announcement';
-import ResetButton from './components/ResetButton';
 
-export function Main({ footer, cement, button}) {
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="menu col-md-12 d-flex justify-content-center">
-                        <h1>Tic Tac Toe</h1>
-                        <Announcement footer={footer}/>
-                        <ResetButton footer={footer}/>
-                    </div>
-                </div>
-                <Footer footer={footer} />
-            </div>
-        </>
-    )
+export function Main({ footer }) {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [whoIsNext, setwhoIsNext] = useState(true);
+  const winner = whoIsWinner(history[stepNumber]);
+  const xo = whoIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xo;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setwhoIsNext(!whoIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setwhoIsNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : "Go to Start";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <h3>{winner ? "Winner: " + winner : "Next Player: " + xo}</h3>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+      </div>
+      <Footer footer={footer} />
+    </>
+  )
 }
